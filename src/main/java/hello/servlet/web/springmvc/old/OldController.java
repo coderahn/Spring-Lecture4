664e6665d3fa48d28e42f1943977716fadd0c959
ash2748 <max3535@naver.com>
2022-06-08 07:38:33 +0900

    스프링 MVC 구조 이해 - 핸들러매핑과 핸들러어댑터(Controller 인터페이스, HttpRequestHandler 처리과정)

---
+++ src/main/java/hello/servlet/web/springmvc/old/OldController.java
@@ -0,0 +1,19 @@
+package hello.servlet.web.springmvc.old;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+//@Controller 이전에 사용한 Controller 인터페이스
+//@Component : 스프링 빈 이름을 URL로 맞추어 사용 가능
+@Component("/springmvc/old-controller")
+public class OldController implements Controller {
+    @Override
+    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        System.out.println("OldController.handleRequest");
+        return null;
+    }
+}
